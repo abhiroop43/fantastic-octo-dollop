50a96addf182bcadedf91a349fd38b470e66f25f
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2022-04-23 00:42:08 +0400

    completed top toolbar and side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'recruitment-ui';
+
+  @ViewChild('drawer', { read: ElementRef }) myTestDiv!: ElementRef<MatDrawer>;
+
+  ngAfterViewInit(): void {
+    console.log(this.myTestDiv.nativeElement);
+  }
 }
